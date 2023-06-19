718ee0d590b6314155cb2f637f8a5ddeb4865541
Omar Diab <omar_diab_1995@yahoo.com>
2023-06-19 18:03:57 +0300

    Load URL when load view.
    Load URL when load view and allow to navigate to other websites from Tamatem page.
    Handel possible errors in model

--- Tamatem web App/webViewController.swift
+++ Tamatem web App/webViewController.swift
@@ -37,7 +37,22 @@ class webViewController: UIViewController, WKNavigationDelegate {
         
         forwardBtn.isEnabled = webView.canGoForward
         forwardBtn.action = #selector(WKWebView.goForward)
+                        
+        guard URLModel != nil,
+            let url = URLModel?.getHomeURL() else {
+            let alert = UIAlertController(title: "Error", message:  "Unable to open the website", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default,  handler: {_ in
+                self.dismiss(animated: true)
+            }))
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
+            return
+        }
         progressView.isHidden = false
+        webView.load(URLRequest(url: url))
+    }
+    
     @IBAction func closeAction(_ sender: Any) {
         self.dismiss(animated: true)
     }
@@ -45,6 +60,15 @@ class webViewController: UIViewController, WKNavigationDelegate {
     @IBAction func reloadAction(_ sender: Any) {
         webView.reload()
     }
+    
+    //OD: implement the decidePolicyFor method. We pull out the host of the URL that was requested, then we call the decisionHandler() .allow to open all websites also we check if targetFrame == nil, this mean we changed to anothe rwebsite.
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if navigationAction.targetFrame == nil {
+            webView.load(navigationAction.request)
+        }
+        decisionHandler(.allow)
+    }
+    
     //OD: Monitoring page loading progress and navigation ability
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == #keyPath(WKWebView.canGoForward)  {
