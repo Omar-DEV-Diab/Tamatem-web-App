b9b1cd9d933d5e1bc1b4ebd85750f0cf469c2dac
Omar Diab <omar_diab_1995@yahoo.com>
2023-06-19 18:01:38 +0300

    Implement  progress bar.

--- Tamatem web App/webViewController.swift
+++ Tamatem web App/webViewController.swift
@@ -20,17 +20,24 @@ class webViewController: UIViewController, WKNavigationDelegate {
     @IBOutlet weak var forwardBtn: UIBarButtonItem!
     @IBOutlet weak var reloadBtn: UIBarButtonItem!
     
+    @IBOutlet weak var progressView: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         webView.navigationDelegate = self
         webView.allowsBackForwardNavigationGestures = true
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
+        
+        progressView.progressTintColor = .red
+        
         backBtn.isEnabled = webView.canGoBack
         backBtn.action = #selector(WKWebView.goBack)
         
         forwardBtn.isEnabled = webView.canGoForward
         forwardBtn.action = #selector(WKWebView.goForward)
+        progressView.isHidden = false
     @IBAction func closeAction(_ sender: Any) {
         self.dismiss(animated: true)
     }
@@ -48,6 +55,21 @@ class webViewController: UIViewController, WKNavigationDelegate {
             backBtn.isEnabled = webView.canGoBack
             return
         }
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    //OD: Show the progress bar when start loading
+    func webView(_: WKWebView, didStartProvisionalNavigation _: WKNavigation!) {
+        if progressView.isHidden {
+            progressView.isHidden = false
+        }
+    }
+            
+    //OD: Hide the progress bar when finish loading
+    internal func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        progressView.isHidden = true
     }
     }
 }
